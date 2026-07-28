b7e68e374d642ce9e5f3523542fbf40451eb29da
nawrin30 <nawrintarannum30@gmail.com>
2026-07-28 18:35:08 +0600

    document

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let todos = JSON.parse(localStorage.getItem('pro_todo_list')) || [];
+let currentFilter = 'all';
+
+
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+const emptyState = document.getElementById('empty-state');
+const filterBtns = document.querySelectorAll('.tab-btn');
+const itemsLeft = document.getElementById('items-left');
+const clearCompletedBtn = document.getElementById('clear-completed');
+const completedCount = document.getElementById('completed-count');
+const totalCount = document.getElementById('total-count');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderTodos();
+    setupEventListeners();
+});
 
 
 
